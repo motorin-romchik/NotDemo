c3dad5510bad24a79e7554bc656f2a686c1eb58b
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-03-22 18:46:02 +0700

    Initial commit

---
+++ src/NotDemo.java
@@ -0,0 +1,22 @@
+public class NotDemo {
+    public static void main(String args[]){
+        byte b = -34;
+
+        for (int t = 128; t > 0 ;t/=2) {
+            if ((b & t) != 0)
+                System.out.print("1 ");
+            else
+                System.out.print("0 ");
+        }
+            System.out.println();
+            b = (byte) ~b;
+            for (int t=128 ; t > 0; t/=2){
+                if ((b &t) != 0)
+                    System.out.print("1 ");
+                else
+                    System.out.print("0 ");
+
+            }
+
+        }
+    }
